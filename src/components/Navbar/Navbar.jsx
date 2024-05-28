83cce77a31e4aceb7da49190a6a5063e65b6a774
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-05-28 23:52:56 +0100

    Top Navbar Done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import design from './Navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <nav className={design.container}>
+            <div>
+                Navbar
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
