59b64d2efa000fa3f16fc33aa88f926c91dc0a92
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-05-31 16:37:54 +0100

    Featured Products Section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 import BestServices from "../components/BestServices/BestServices";
 import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar/Navbar"
-import Products from "../components/Products/Products";
 import Topnav from "../components/Topnav/Topnav"
+import Products from "./featuredProduct/Products";
 
 const Home = () => {
     return (
