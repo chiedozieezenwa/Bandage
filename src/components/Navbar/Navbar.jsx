1c4c52747c2db9e401cb174b92c5fee5b667d3d5
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-05-31 01:04:05 +0100

    Hero Page and Hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,20 @@ import person from '../../assets/person.svg'
 import search from '../../assets/search.svg'
 import cart from '../../assets/cart.svg'
 import heart from '../../assets/heart.svg'
+import { useState } from 'react'
+import classNames from 'classnames'
+import searchicon from '../../assets/searchicon.svg'
+import shopcart from '../../assets/shopcart.svg'
+import toggle from '../../assets/toggle.svg'
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className={design.container}>
             <section className={design.title}>
@@ -20,13 +32,24 @@ const Navbar = () => {
                     <li>Contact</li>
                     <li>Pages</li>
                 </ul>
-                <ul className={design.signup}>
+                <ul className={classNames(design.signup, design.list_items)}>
                     <li><img src={person} alt="person" /> Login / Register</li>
                     <li><img src={search} alt="search" /></li>
                     <li><img src={cart} alt="View Cart" /> 1</li>
                     <li><img src={heart} alt="Reaction" /> 1</li>
                 </ul>
             </section>
+            <section className={design['toggle-menu']}>
+                <img src={searchicon} alt="Click to search" />
+                <img src={shopcart} alt="Add to cart" />
+                <img src={toggle} alt="toggle menu" onClick={toggleMenu}/>
+            </section>
+            <ul className={classNames(design.toggle_items, { [design['is-open']]: isOpen })}>
+                <li>Home</li>
+                <li>Product</li>
+                <li>Pricing</li>
+                <li>Contact</li>
+            </ul>
         </nav>
     )
 }
