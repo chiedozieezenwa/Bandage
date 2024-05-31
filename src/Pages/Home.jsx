ff7c90a515e1289b32c7c6ddc489b73bd6409d76
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-05-31 02:21:27 +0100

    Best Services Section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
+import BestServices from "../components/BestServices/BestServices";
 import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar/Navbar"
+import Products from "../components/Products/Products";
 import Topnav from "../components/Topnav/Topnav"
 
 const Home = () => {
@@ -8,6 +10,8 @@ const Home = () => {
             <Topnav />
             <Navbar />
             <Hero />
+            <Products />
+            <BestServices />
         </div>
     )
 }
